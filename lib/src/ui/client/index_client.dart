7bb6a560e7006e15d4c57c4400c7e02de70a1e87
j_nar <jquezadac@toluca.tecnm.mx>
2021-06-20 12:56:33 -0500

    Lectura de sensores

--- lib/src/ui/client/index_client.dart
+++ lib/src/ui/client/index_client.dart
@@ -8,6 +8,53 @@ import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:fl_chart/fl_chart.dart';
 import 'dart:async';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+Map<String, dynamic> map ;
+Future<datosMes> datosMesget() async {
+  final response =
+  await http.get('http://besoftware.com.mx/iot/datosmovil');
+
+  if (response.statusCode == 200) {
+    // Si la llamada al servidor fue exitosa, analiza el JSON
+    map=json.decode(response.body);
+    return datosMes.fromJson(json.decode(response.body));
+  } else {
+    // Si la llamada no fue exitosa, lanza un error.
+    throw Exception('Failed to load post');
+  }
+}
+
+Future<List> tablaGastos() async {
+  final response = await http.get("http://besoftware.com.mx/iot/tablagastos");
+  return json.decode(response.body);
+}
+
+
+
+
+class datosMes {
+
+  final String wattsmes;
+  final String litrosmes;
+  final String fgas;
+  final String aguainm;
+  final String eli;
+
+
+  datosMes({this.wattsmes, this.litrosmes,this.fgas,this.aguainm,this.eli});
+
+  factory datosMes.fromJson(Map<String, dynamic> json) {
+    return datosMes(
+      wattsmes: json['wattsmes'],
+      litrosmes: json['litrosmes'],
+      fgas: json['fgas'],
+      aguainm: json['aguainm'],
+      eli: json['eli'],
+    );
+  }
+}
+
 
 class IndexClient extends StatefulWidget {
   final String correoElectronico;
@@ -29,29 +76,9 @@ class _IndexClientState extends State<IndexClient> {
   String telefono;
   String foto;
   String id;
-  List<PieChartSectionData> sectionsChart = [
-    PieChartSectionData(
-      value: 20,
-      title: "Agua",
-      showTitle: true,
-      color: Colors.orange,
-      radius: 100,
-    ),
-    PieChartSectionData(
-      value: 45,
-      title: "Gas",
-      showTitle: true,
-      color: Colors.blue,
-      radius: 100,
-    ),
-    PieChartSectionData(
-      value: 35,
-      title: "Electricidad",
-      showTitle: true,
-      color: Colors.red,
-      radius: 100,
-    ),
-  ];
+
+
+
   Future<Null> _logoutUser() async {
     SharedPreferences prefs;
     prefs = await SharedPreferences.getInstance();
@@ -59,9 +86,48 @@ class _IndexClientState extends State<IndexClient> {
     // ignore: deprecated_member_use
     prefs.commit();
   }
-
+  Future<datosMes> data;
+  double a;
+  double b;
+  String errores;
   @override
   void initState() {
+    data=datosMesget();
+    String elimes = "50.0";
+    String litmes="50.0";
+     FutureBuilder<datosMes>(
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            litmes=snapshot.data.litrosmes;
+            return Text(snapshot.data.litrosmes);
+          } else if (snapshot.hasError) {
+            litmes="50.0";
+            return Text("50.0");
+          }
+        }).createElement();
+
+    FutureBuilder<datosMes>(
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            elimes = snapshot.data.wattsmes;
+            return Text(snapshot.data.wattsmes);
+          } else if (snapshot.hasError) {
+            elimes = "50.0";
+            return Text("50.0");
+          }
+        });
+try{
+    elimes = map["wattsmes"];
+    litmes = map["litrosmes"];}catch(e){
+
+}
+    double x,y;
+
+    x = double.parse(elimes);
+    y = double.parse(litmes);
+    double inter = x+y;
+    a= (x*100)/inter;
+    b= (y*100)/inter;
     List<dynamic> clienteID = [];
     List<dynamic> clienteData = [];
     clientRef
@@ -94,6 +160,23 @@ class _IndexClientState extends State<IndexClient> {
 
   @override
   Widget build(BuildContext context) {
+
+    List<PieChartSectionData> sectionsChart = [
+      PieChartSectionData(
+        value: b,
+        title: "Agua",
+        showTitle: true,
+        color: Colors.orange,
+        radius: 100,
+      ),
+      PieChartSectionData(
+        value: a,
+        title: "Electricidad",
+        showTitle: true,
+        color: Colors.red,
+        radius: 100,
+      ),
+    ];
     return Scaffold(
         appBar: AppBar(
             title: nombre != null ? Text(nombre) : Text('Be IoT System'),
@@ -148,7 +231,104 @@ class _IndexClientState extends State<IndexClient> {
                               padding: EdgeInsets.all(5),
                               child: Align(
                                   alignment: Alignment.topLeft,
-                                  child: Text('Energía gastada en el mes: 1250 w',
+                                  child: FutureBuilder<datosMes>(
+                                    future: data,
+                                    builder: (context, snapshot) {
+                                      if (snapshot.hasData) {
+                                        return Text("Gatos inmediato de electricidad: " + snapshot.data.eli + "W/hr");
+                                      } else if (snapshot.hasError) {
+                                        return Text("${snapshot.error}");
+                                      }
+
+                                      // Por defecto, muestra un loading spinner
+                                      return CircularProgressIndicator();
+                                    },
+                                  ),
+                              ),
+
+                            ),
+                          ]))),
+                  Card(
+                      child: Padding(
+                          padding: EdgeInsets.all(10),
+                          child: Column(mainAxisSize: MainAxisSize.min, children: [
+                            Padding(
+                              padding: EdgeInsets.all(5),
+                              child: Align(
+                                  alignment: Alignment.topLeft,
+                                  child:  FutureBuilder<datosMes>(
+                                    future: data,
+                                    builder: (context, snapshot) {
+                                      if (snapshot.hasData) {
+                                        return Text("Gatos mensual de electricidad: " + snapshot.data.wattsmes + "W");
+                                      } else if (snapshot.hasError) {
+                                        return Text("${snapshot.error}");
+                                      }
+
+                                      // Por defecto, muestra un loading spinner
+                                      return CircularProgressIndicator();
+                                    },
+                                  ),
+                              ),
+                            ),
+                          ]))),
+                  Card(
+                      child: Padding(
+                          padding: EdgeInsets.all(10),
+                          child: Column(mainAxisSize: MainAxisSize.min, children: [
+                            Padding(
+                              padding: EdgeInsets.all(5),
+                              child: Align(
+                                  alignment: Alignment.topLeft,
+                                  child:  FutureBuilder<datosMes>(
+                                    future: data,
+                                    builder: (context, snapshot) {
+                                      if (snapshot.hasData) {
+                                        return Text("Gatos mensual de agua: " + snapshot.data.litrosmes + "L");
+                                      } else if (snapshot.hasError) {
+                                        return Text("${snapshot.error}");
+                                      }
+
+                                      // Por defecto, muestra un loading spinner
+                                      return CircularProgressIndicator();
+                                    },
+                                  ),
+                              ),
+                            ),
+                          ]))),
+                  Card(
+                      child: Padding(
+                          padding: EdgeInsets.all(10),
+                          child: Column(mainAxisSize: MainAxisSize.min, children: [
+                            Padding(
+                              padding: EdgeInsets.all(5),
+                              child: Align(
+                                  alignment: Alignment.topLeft,
+                                  child:  FutureBuilder<datosMes>(
+                                    future: data,
+                                    builder: (context, snapshot) {
+                                      if (snapshot.hasData) {
+                                        return Text("Gatos inmediato de agua: " + snapshot.data.aguainm + "L/hr");
+                                      } else if (snapshot.hasError) {
+                                        return Text("${snapshot.error}");
+                                      }
+
+                                      // Por defecto, muestra un loading spinner
+                                      return CircularProgressIndicator();
+                                    },
+                                  ),
+                              ),
+                            ),
+                          ]))),
+                  Card(
+                      child: Padding(
+                          padding: EdgeInsets.all(10),
+                          child: Column(mainAxisSize: MainAxisSize.min, children: [
+                            Padding(
+                              padding: EdgeInsets.all(5),
+                              child: Align(
+                                  alignment: Alignment.topLeft,
+                                  child: Text('Cambios en gastos del mes: 38 %',
                                       style: TextStyle(fontSize: 15.0))),
                             ),
                           ]))),
@@ -160,57 +340,20 @@ class _IndexClientState extends State<IndexClient> {
                               padding: EdgeInsets.all(5),
                               child: Align(
                                   alignment: Alignment.topLeft,
-                                  child: Text(
-                                      'Gasto inmediato de electricidad: 1500 wh/r',
-                                      style: TextStyle(fontSize: 15.0))),
-                            ),
-                          ]))),
-                  Card(
-                      child: Padding(
-                          padding: EdgeInsets.all(10),
-                          child: Column(mainAxisSize: MainAxisSize.min, children: [
-                            Padding(
-                              padding: EdgeInsets.all(5),
-                              child: Align(
-                                  alignment: Alignment.topLeft,
-                                  child: Text('Agua gastada en el mes: 300 L',
-                                      style: TextStyle(fontSize: 15.0))),
-                            ),
-                          ]))),
-                  Card(
-                      child: Padding(
-                          padding: EdgeInsets.all(10),
-                          child: Column(mainAxisSize: MainAxisSize.min, children: [
-                            Padding(
-                              padding: EdgeInsets.all(5),
-                              child: Align(
-                                  alignment: Alignment.topLeft,
-                                  child: Text('Gasto inmediato de agua: 2 L/m',
-                                      style: TextStyle(fontSize: 15.0))),
-                            ),
-                          ]))),
-                  Card(
-                      child: Padding(
-                          padding: EdgeInsets.all(10),
-                          child: Column(mainAxisSize: MainAxisSize.min, children: [
-                            Padding(
-                              padding: EdgeInsets.all(5),
-                              child: Align(
-                                  alignment: Alignment.topLeft,
-                                  child: Text('Gasto en el mes redujo en: 38 %',
-                                      style: TextStyle(fontSize: 15.0))),
-                            ),
-                          ]))),
-                  Card(
-                      child: Padding(
-                          padding: EdgeInsets.all(10),
-                          child: Column(mainAxisSize: MainAxisSize.min, children: [
-                            Padding(
-                              padding: EdgeInsets.all(5),
-                              child: Align(
-                                  alignment: Alignment.topLeft,
-                                  child: Text('Fugas de gas encontradas: 0',
-                                      style: TextStyle(fontSize: 15.0))),
+                                  child:  FutureBuilder<datosMes>(
+                                    future: data,
+                                    builder: (context, snapshot) {
+                                      if (snapshot.hasData) {
+                                        return Text("Fugas de gas el día de hoy: " + snapshot.data.fgas );
+                                      } else if (snapshot.hasError) {
+                                        return Text("${snapshot.error}");
+                                      }
+
+                                      // Por defecto, muestra un loading spinner
+                                      return CircularProgressIndicator();
+                                    },
+                                  ),
+                              ),
                             ),
                           ]))),
                   Card(
@@ -225,6 +368,7 @@ class _IndexClientState extends State<IndexClient> {
                                       style: TextStyle(fontSize: 15.0))),
                             ),
                           ]))),
+
                   Container(
                       width: MediaQuery.of(context).size.width,
                       height: 300,
@@ -253,26 +397,24 @@ class _IndexClientState extends State<IndexClient> {
                     sortColumnIndex: 2,
                     sortAscending: false,
                     columns: [
-                      DataColumn(label: Text("Luz")),
-                      DataColumn(label: Text("Agua")),
+                      DataColumn(label: Text("id")),
+                      DataColumn(label: Text("Año")),
                       DataColumn(label: Text("Bimestre"), numeric: true),
+                      DataColumn(label: Text("Electricidad")),
+                      DataColumn(label: Text("Agua")),
+                      DataColumn(label: Text("Gas")),
+                      DataColumn(label: Text("Cliente")),
                     ],
                     rows: [
                       DataRow(selected: true, cells: [
-                        DataCell(Text("200")),
-                        DataCell(Text("150")),
-                        DataCell(Text("6/2020"))
-                      ]),
-                      DataRow(cells: [
+                        DataCell(Text("1")),
+                        DataCell(Text("2021")),
+                        DataCell(Text("3")),
+                        DataCell(Text("250")),
                         DataCell(Text("120")),
-                        DataCell(Text("125")),
-                        DataCell(Text("1/2021"))
+                        DataCell(Text("0")),
+                        DataCell(Text("1"))
                       ]),
-                      DataRow(cells: [
-                        DataCell(Text("120")),
-                        DataCell(Text("125")),
-                        DataCell(Text("2/2021"))
-                      ])
                     ],
                   ),
                 ]))));
